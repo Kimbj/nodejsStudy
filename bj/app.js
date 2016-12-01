efb25f2fb017f83861794e0386cadd241e302de3
kbj <4xjida@naver.com>
2016-12-02 02:27:43 +0900

    MONGO DB 연동완료 7장

--- bj/app.js
+++ bj/app.js
@@ -1,22 +1,63 @@
 var express = require('express');
 var path = require('path');
-var  app = express();
+var app = express();
+var mongoose = require('mongoose');
 
-app.set("view engine","ejs");
-app.use(express.static(path.join(__dirname,'/public')));
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
 
-var data={count:0};
-
-app.get('/',function (req,res){
-  data.count++;
-  res.render('my_first_ejs',data);
+db.once("open",function(){
+  console.log("DB connected!");
 });
 
-app.get('/reset',function (req,res){
-  data.count=0;
-  res.render('my_first_ejs',data);
+db.on("error",function(err){
+  console.log("DB ERROR :",err);
 });
 
-app.listen(3000,function(){
-  console.log('Server On!!!!!');
+var dataSchema = mongoose.Schema({
+  name:String,
+  count:Number
+});
+
+var Data = mongoose.model('data',dataSchema);
+
+Data.findOne({name:"myData"},function(err,data){
+  if(err) return console.log("Data ERROR:", err);
+  if(!data){
+    Data.create({name:"myData",count:0},function(err,data){
+      if(err) return console.log("Data ERROR:",err);
+    });
+  }
+});
+
+app.set("view engine",'ejs');
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/', function (req,res){
+  Data.findOne({name:"myData"},function(err,data){
+    if(err) return console.log("Data ERROR:", err);
+    data.count++;
+    data.save(function(err){
+      res.render('my_first_ejs',data);
+    });
+  });
+});
+
+app.get('/reset',function(req,res){
+  // data.count=0;
+  // res.render('my_first_ejs',data);
+});
+
+app.get('/set/count',function(req,res){
+  // if(req.query.count) data.count = req.query.count;
+  // res.render('my_first_ejs',data);
+});
+
+app.get('/set/:num',function(req,res){
+  // data.count=req.params.num;
+  // res.render('my_first_ejs',data);
+});
+
+app.listen(3000, function(){
+  console.log('Server On!');
 });
