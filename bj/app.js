4eebe93895b6cb1be85f12d0253652381dff930e
kbj <applecubic@tourabaksa.co.kr>
2016-12-02 13:36:57 +0900

    9장 진행중

--- bj/app.js
+++ bj/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
