8ba17efabf7a4086080ffb6a0d1317e241876f6a
kbj <4xjida@naver.com>
2016-12-08 03:00:50 +0900

    9-2장 진행중

--- bj/app.js
+++ bj/app.js
@@ -3,8 +3,9 @@ var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
-mongoose.connect(process.env.MONGO_DB);
+mongoose.connect("mongodb://cubic:k12345678@ds037824.mlab.com:37824/applecubic");
 var db = mongoose.connection;
 
 db.once("open",function(){
@@ -15,50 +16,67 @@ db.on("error",function(err){
   console.log("DB ERROR :",err);
 });
 
-var dataSchema = mongoose.Schema({
-  name:String,
-  count:Number
+// model Setting
+var postSchema = mongoose.Schema({
+  title: {type:String, required:true},
+  body: {type:String, required:true},
+  createAt: {type:Date, default:Date.now},
+  updatedAt: Date
 });
+var Post = mongoose.model('post',postSchema);
 
-var Data = mongoose.model('data',dataSchema);
-
-Data.findOne({name:"myData"},function(err,data){
-  if(err) return console.log("Data ERROR:", err);
-  if(!data){
-    Data.create({name:"myData",count:0},function(err,data){
-      if(err) return console.log("Data ERROR:",err);
-    });
-  }
-});
-
+// view Setting
 app.set("view engine",'ejs');
-app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/', function (req,res){
-  Data.findOne({name:"myData"},function(err,data){
-    if(err) return console.log("Data ERROR:", err);
-    data.count++;
-    data.save(function(err){
-      res.render('my_first_ejs',data);
-    });
+// set middlewares
+app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method")); // DELETE, PUT method 사용
+
+// set routes
+app.get('/posts', function(req,res){
+  Post.find({}).sort('-createdAt').exec(function(err,posts){
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/index", {data:posts});
+  });
+}); // index
+
+app.get('/posts/new',function(req,res){
+  res.render("posts/new");
+}); // new
+
+app.post('/posts', function(req,res){
+  Post.create(req.body.post, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.rediect('/posts');
+  });
+}); // create
+
+app.get('/posts/:id',function(req,res){
+  Post.findById(req.params.id,function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/show", {data:post});
   });
 });
 
-app.get('/reset',function(req,res){
-  // data.count=0;
-  // res.render('my_first_ejs',data);
+
+app.put('/posts/:id',function(req,res){
+  req.body.post.updateAt=Date.now();
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, data:post._id+" updated"});
+  });
 });
 
-app.get('/set/count',function(req,res){
-  // if(req.query.count) data.count = req.query.count;
-  // res.render('my_first_ejs',data);
+app.delete('',function(req,res){
+  Post.findByIdAndRemove(req.params.id, req.body.post, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.redirect('/posts');
+  });
 });
 
-app.get('/set/:num',function(req,res){
-  // data.count=req.params.num;
-  // res.render('my_first_ejs',data);
-});
-
+// start server
 app.listen(3000, function(){
   console.log('Server On!');
 });
